33a7845d13ccd153605f0cfeaa88da8cd561ef84
ZAHUNG <hunggia036@gmail.com>
2025-09-26 22:03:17 +0700

    done 16,17,18,19,20

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,90 @@
+const UserService = require("../services/UserService");
+
+const createUser = async(req, res) => {
+    try {
+        // console.log(req.body)
+        const { name, email, password, confirmPassword, phone } = req.body;
+        const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isCheckEmail = reg.test(email)
+        if (!name || !email || !password || !confirmPassword || !phone ) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        }else if(!isCheckEmail){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            })
+        }else if(password !== confirmPassword){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirmPassword'
+            })
+        }
+
+        const response = await UserService.createUser(req.body)
+        return res.status(200).json(response)
+    }catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const loginUser = async(req, res) => {
+    try {
+        // console.log(req.body)
+        const { name, email, password, confirmPassword, phone } = req.body;
+        const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isCheckEmail = reg.test(email)
+        if (!name || !email || !password || !confirmPassword || !phone ) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        }else if(!isCheckEmail){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            })
+        }else if(password !== confirmPassword){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirmPassword'
+            })
+        }
+
+        const response = await UserService.loginUser(req.body)
+        return res.status(200).json(response)
+    }catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const updateUser = async(req, res) => {
+    try {
+        const userId = req.params.id
+        const data = req.body
+        if(!userId){
+            returnres.status(200).json({
+                status: 'ERR',
+                message: 'The userId is required'
+            })
+        }
+        console.log('userId', userId)
+        const response = await UserService.updateUser(userId, data);
+        return res.status(200).json(response)
+    }catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+} 
+module.exports ={
+    createUser,
+    loginUser,
+    updateUser
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require('body-parser')
 dotenv.config();
 
-//require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001
@@ -11,14 +12,19 @@ const port = process.env.PORT || 3001
 app.get('/', (req,res) => {
      res.send('Hello anh em xoiws')
 })
+app.use(bodyParser.json());
 
-mongoose.connect(`mongodb+srv://tgh:${process.env.MONGO_DB}@cluster0.lf2nfzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+routes(app)
+
+mongoose.connect(`${process.env.MONGO_DB}`)
 .then(() =>{
     console.log('Connected to MongoDB');
 })
 .catch((err) =>{
     console.log(err)
 })
+
+
 
 app.listen(port, () =>{
     console.log('Server is running on port:', + port);
