0b86bf30aa8f06d64492ab2221332db7bc204ce8
Thái Xuân Hoàng <txhoang04@gmail.com>
2025-10-02 01:01:11 +0700

    Add files via upload
    #23, 24 done

---
+++ backend/src/routes/ProductRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router()
+const ProductController = require("../controllers/ProductController");
+const { authMiddlewear } = require("../middleware/authMiddlewear");
+
+router.post('/create', ProductController.createProduct);
+router.put('/update/:id',authMiddlewear, ProductController.updateProduct);
+router.get('/details/:id', ProductController.getDetailsProduct);
+router.delete('/delete/:id', ProductController.deleteProduct);
+router.get('/all', ProductController.getAllProduct);
+
+module.exports = router;
