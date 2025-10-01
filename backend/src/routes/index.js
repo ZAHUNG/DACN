a2b05ade18626aafefbc26308c699c3a5b7a5bf0
Thái Xuân Hoàng <txhoang04@gmail.com>
2025-10-02 01:04:20 +0700

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ const UserRouter = require("./UserRouter");
 
 const routes = (app) => {
     app.use('/api/user', UserRouter)
+    app.use('/api/product', ProductRouter)
 }
 
 module.exports = routes;
